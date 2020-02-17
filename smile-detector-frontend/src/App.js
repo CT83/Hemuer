a3dcf56d4e0533fc736c38fc58ff9c40fcad6712
Rohan Sawant <rohansawantct83@gmail.com>
2020-02-17 12:20:20 +0530

    Created react component and refactor

--- smile-detector-frontend/src/App.js
+++ smile-detector-frontend/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SmileDetector from './components/SmileDetector'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render(params) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+            </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SmileDetector></SmileDetector>
+          </a>
+        </header>
+      </div>
+    )
+  }
 }
+
 
 export default App;
---
+++ smile-detector-frontend/src/components/SmileDetector.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SmileDetector extends Component {
+
+    render() {
+        return (
+            <React.Fragment>
+                <h1>This is the face detector!</h1>
+            </React.Fragment>
+        )
+    }
+}
+
+export default SmileDetector;
